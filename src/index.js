4c57d1832c583c1bd091a8c1b1570a759bdbf6a3
rajas19-ita <rajaskadge@gmail.com>
2024-01-23 13:39:04 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ const projectionModel = require("./models/projection");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://insightsdb.netlify.app/",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+    })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
